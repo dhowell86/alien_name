0d304dba430d1f60af088b2db3ea3e753a293d6f
dhowell86 <dhowell86@gmail.com>
2022-01-01 06:09:11 -0500

    added names to arrays

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const firstSyllables = [];
-const secondSyllables = [];
-const thirdSyllables = [];
+const firstSyllables = ['Dat', 'Omni', 'Darth', 'Mel', 'Tun', 'Et', 'Ich', 'Bar', 'Ooop', 'Bok', 'Shan', 'Pharm', 'Chitty-'];
+const secondSyllables = ['enchi', 'noo', ' bat', 'el', 'ma', 'la', 'ta', 'ete', ' ', '-', 'mo', 'let', 'im', 'labra', 'parma', 'Chitty'];
+const thirdSyllables = [' ', 'lada', 'loodle', 'poppadun', 'toot', 'poot', 'zoot', 'telen', 'lata', 'bootie', ', Jr.', ', III', ', Sr.', 'ladle', 'ibble', 'doodle', 'Bang-Bang'];
 
 const randomSelector = array => {
-    let i = Math.floor(Math.random()*array.length)
+    let i = Math.floor(Math.random()*(array.length))
     return array[i];
 }
 
